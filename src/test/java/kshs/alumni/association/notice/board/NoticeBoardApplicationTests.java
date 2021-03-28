162f75880c1edc75c684360ced1a47b1532df8b0
정시훈 <eekshs16@gmail.com>
2021-03-28 20:51:07 +0900

    첫 커밋

---
+++ src/test/java/kshs/alumni/association/notice/board/NoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kshs.alumni.association.notice.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
